7a63dd57213d66a499590c889bd2a1500420ce21
Deepak <deepakworkpc@gmail.com>
2024-11-15 03:10:30 +0530

    feat: maze kinda works

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let maze = [[2, 2]];
-const mazeMap = document.querySelector(".maze");
+let maze = [[12, 16]];
+const mazeMap = document.querySelector(".maze-map");
 let width = 27;
 
 const createMaze = () => {
@@ -24,16 +24,25 @@ const coordsDeterminer = (xAdd, yAdd) => {
 
 // let topCoords = [maze.at(-1)[0], maze.at(-1)[1]]
 // console.log(coordsDeterminer(1, 0))
-for (let index = 0; index < 10; index++) {
+createMaze();
+for (let index = 0; index < 120; index++) {
     
     let surroundingCoords = [
         coordsDeterminer(0, -1), 
         coordsDeterminer(1, 0), 
-        coordsDeterminer(0, -1), 
+        coordsDeterminer(0, 1), 
         coordsDeterminer(-1, 0)
     ]
 
-    console.log(surroundingCoords)
+    // console.log(surroundingCoords)
+
+    const randomElement = surroundingCoords[Math.floor(Math.random() * surroundingCoords.length)];
+
+    console.log(randomElement.toString())
+    document.getElementById(randomElement.toString()).style.backgroundColor = "red";
+
+    maze.push(randomElement)
+    console.log(maze)
 } 
 
 // {
