25259b5a86580ca800d7ba9eb2c4fc4052092d42
Deepak <deepakworkpc@gmail.com>
2024-11-16 00:45:57 +0530

    feat: add the mech to check for negative elems inside the subarray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let maze = [[12, 16]];
+let maze = [[0, 0]];
 const mazeMap = document.querySelector(".maze-map");
 let width = 27;
 
@@ -25,7 +25,7 @@ const coordsDeterminer = (xAdd, yAdd) => {
 // let topCoords = [maze.at(-1)[0], maze.at(-1)[1]]
 // console.log(coordsDeterminer(1, 0))
 createMaze();
-for (let index = 0; index < 120; index++) {
+for(let i = 0; i < 10; i++) {
     
     let surroundingCoords = [
         coordsDeterminer(0, -1), 
@@ -34,28 +34,21 @@ for (let index = 0; index < 120; index++) {
         coordsDeterminer(-1, 0)
     ]
 
-    // console.log(surroundingCoords)
+    let r = surroundingCoords.filter(arr => arr.every(x => x >= 0))
+    // surrounding coords should not be at the already placed maze
 
-    const randomElement = surroundingCoords[Math.floor(Math.random() * surroundingCoords.length)];
+    const filteredArray = maze.filter(element => !surroundingCoords.includes(element));
 
-    console.log(randomElement.toString())
-    document.getElementById(randomElement.toString()).style.backgroundColor = "red";
+    // console.log(r);
+    console.log(filteredArray)
+
+    const randomElement = filteredArray[Math.floor(Math.random() * filteredArray.length)];
+
+    // console.log(randomElement.toString())
+    if (randomElement != undefined) {
+        document.getElementById(randomElement.toString()).style.backgroundColor = "red";
+    }
 
     maze.push(randomElement)
-    console.log(maze)
+    // console.log(maze)
 } 
-
-// {
-//     let surroundingCoords = [
-//         coordsDeterminer(0, -1), 
-//         coordsDeterminer(1, 0), 
-//         coordsDeterminer(0, -1), 
-//         coordsDeterminer(-1, 0)
-//     ]
-
-//     let filteredCoords = surroundingCoords.filter(coords => {
-//         return coords.every(value => value >= 0);
-//       });
-      
-//       console.log(filteredCoords);
-//       }
